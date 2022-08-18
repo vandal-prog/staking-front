7e94afc496d5db7bd0d1f767d01ed9a9230beabe
Ephraim O. Oladepo <103599981+Flame-eth@users.noreply.github.com>
2022-08-18 14:57:16 +0100

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { ethers } from "ethers";
 
+import Staking from "../../BlockchainData/build/Staking.json";
+
+import USDT from "../../BlockchainData/build/IERC20.json";
+
 import { AiOutlineClose } from "react-icons/ai";
 import { HiMenuAlt4 } from "react-icons/hi";
 
@@ -14,6 +18,8 @@ import { shortenAddress } from "../../utils/shortenAddress";
 import {
   setCurrentAccount,
   setOnChainBalance,
+  hasStaked,
+  hasPledged,
 } from "../../redux/user/user.actions";
 
 const NAV__LINKS = [
@@ -40,7 +46,13 @@ const NAV__LINKS = [
   },
 ];
 
-const Header = ({ currentAccount, setCurrentAccount, setOnChainBalance }) => {
+const Header = ({
+  currentAccount,
+  setCurrentAccount,
+  setOnChainBalance,
+  hasStaked,
+  hasPledged,
+}) => {
   // const { currentAccount, connectWallet, setOnChainBalance } =
   //   useContext(TransactionContext);
 
@@ -59,10 +71,30 @@ const Header = ({ currentAccount, setCurrentAccount, setOnChainBalance }) => {
         method: "eth_requestAccounts",
       });
 
+      // const contractAddress = "0xfF79f9C507ebA207a02C6c7ce6d13f30DF09d9d2";
+      // const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // const signer = provider.getSigner();
+      // const transactionContract = new ethers.Contract(
+      //   contractAddress,
+      //   Staking.abi,
+      //   signer
+      // );
+
+      // const USDTaddress = "0xfab46e002bbf0b4509813474841e0716e6730136";
+      // const usdtContract = new ethers.Contract(USDTaddress, USDT.abi, signer);
+
+      // console.log("contract loaded");
+
+      // const chainBalance = await usdtContract.balanceOf(accounts[0]);
+
+      // console.log(chainBalance.toString());
+
       // const accounts = await provider.send("eth_requestAccounts", []);
 
       setCurrentAccount(accounts[0]);
-      setOnChainBalance(accounts[0]);
+      await setOnChainBalance(accounts[0]);
+      await hasStaked();
+      await hasPledged();
       // console.log(state);
     } catch (error) {
       console.log(error);
@@ -162,6 +194,8 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   setCurrentAccount: (account) => dispatch(setCurrentAccount(account)),
   setOnChainBalance: (balance) => dispatch(setOnChainBalance(balance)),
+  hasStaked: (stakedBool) => dispatch(hasStaked(stakedBool)),
+  hasPledged: (pledgedBool) => dispatch(hasPledged(pledgedBool)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
