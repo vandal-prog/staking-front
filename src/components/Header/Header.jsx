23046dafc99ffb292c9b94fe699da9d1fe30ce30
Josiah Onche <54504356+vandal-prog@users.noreply.github.com>
2022-08-20 23:57:56 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ import { NavLink, Link } from "react-router-dom";
 
 import { TransactionContext } from "../../context/TransactionContext";
 import { shortenAddress } from "../../utils/shortenAddress";
-import TawkTo from "tawkto-react";
+
 import {
   setCurrentAccount,
   setOnChainBalance,
