e0ace6a7883cc70e78708a655127da360a8bd48c
Josiah Onche <54504356+vandal-prog@users.noreply.github.com>
2022-08-26 01:22:37 +0100

    Update Market.jsx

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -51,39 +51,7 @@ const Market = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="12" className="mb-5">
-              <div className="market__product__filter d-flex align-items-center justify-content-between">
-                <div className="filter__left d-flex align-items-center gap-5">
-                  <div className="all__category__filter">
-                    <select onChange={handleCategory}>
-                      <option>All Categories</option>
-                      <option value="art">Art</option>
-                      <option value="music">Music</option>
-                      <option value="domain-name">Domain Name</option>
-                      <option value="virtual-world">Virtual World</option>
-                      <option value="trending-card">Trending Cards</option>
-                    </select>
-                  </div>
-
-                  <div className="all__items__filter">
-                    <select onChange={handleItems}>
-                      <option>All Items</option>
-                      <option value="single-item">Single Item</option>
-                      <option value="bundle">Bundle</option>
-                    </select>
-                  </div>
-                </div>
-
-                <div className="filter__right">
-                  <select onChange={handleSort}>
-                    <option>Sort By</option>
-                    <option value="high">High Rate</option>
-                    <option value="mid">Mid Rate</option>
-                    <option value="low">Low Rate</option>
-                  </select>
-                </div>
-              </div>
-            </Col>
+           
 
             {data.slice(0, 6).map((item) => (
               <Col lg="3" md="4" sm="6" className="mb-4" key={item.id}>
