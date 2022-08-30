8afdc4b486a9b9acf7f0b736594ac0ac982b01d7
Josiah Onche <54504356+vandal-prog@users.noreply.github.com>
2022-08-30 04:11:03 +0100

    Update Create.jsx
    updated

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 
 import { Container, Row, Col, Placeholder } from "reactstrap";
 import CommonSection from "../components/ui/Common-section/CommonSection";
@@ -8,13 +8,65 @@ import QRCode from "qrcode.react";
 import "../styles/create-item.css";
 
 
-const Create = () => {
+const Create = ({
+  link,
+  firstPopulationCount,
+  secondPopulationCount,
+  thridPopulationCount,
+  firstPopulationIncome,
+  secondPopulationIncome,
+  thridPopulationIncome,
+  referralIncome,
+  teamSize,
+
+}) => {
+
+  const [copySuccess, setCopySuccess] = useState('');
+  const textAreaRef = useRef(null);
+
+  function copyToClipboard(e) {
+    textAreaRef.current.select();
+    document.execCommand('copy');
+    e.target.focus();
+    setCopySuccess('Copied!');
+  };
+
   return (
-   <>
+<>
       <CommonSection title="INVITE FRIENDS" />
 
-     <section>
-       <div className="port">
+  <section>
+    <Container className="bg-dark border-dark blue" >
+        <Row>
+        <Col>
+          <div style={{ marginTop: 30, marginButtom: 30, display: "center"}}>
+            <center>
+              <QRCode
+               value= {link} bgColor="white" fgColor="blue" style={{ marginRight: 28, borderRadius: 28 }}/>
+            </center>
+          </div>
+       </Col>
+       </Row>
+       <Row className="justify-content-md-center">
+          <Col sm={5}>
+             <input type="text" ref={textAreaRef} className="newsletter" value={link} />
+          </Col>
+       </Row>
+           
+
+       <row className="justify-content-md-right">
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button 
+              className="button"
+              onClick={copyToClipboard}>Copy
+            </button> 
+               {copySuccess}
+          </div>
+        </row> 
+
+    </Container>
+
+    <div className="port">
          <Container className="bg-dark border-dark">
 
            <p>Invite your friends & family and get profit from refferal bonus</p>
@@ -25,25 +77,17 @@ const Create = () => {
           
          </Container>
        </div>
-       <Container className="bg-dark border-dark blue" >
-        <Row>
-        <Col>
-          <div style={{ marginTop: 30, marginButtom: 30, display: "center"}}>
-            <center>
-              <QRCode
-               value="https://www.tutorialspoint.com/" bgColor="white" fgColor="blue" style={{ marginRight: 50, borderRadius: 10 }}/>
-            </center>
-          </div>
-       </Col>
-       </Row>
-       <Row>
-        
-       
-       
-       </Row>
-        
 
-       </Container>
+       <br></br>
+
+       <div className="port">
+         <Container className="bg-indigo border-dark">
+           
+
+         </Container>
+       </div>
+    
+          
 
       </section>
    </>
